4a5509990d65f6b21c332d9a2c038b7df2c2ddfe
javierestrada26 <javiestrada14@hotmail.com>
2026-09-16 14:10:47 -0500

    feat: initialize Prisma setup with SQLite and create Product model
    feat: add WASM loaders for Prisma query compilation
    
    feat: implement NestJS microservice bootstrap with validation
    
    feat: create Prisma module and service for database interactions
    
    feat: add DTOs for product creation and updating
    
    feat: implement Product entity and controller for handling requests
    
    feat: develop Products service with CRUD operations and pagination
    
    test: add end-to-end tests for application routes
    
    chore: configure Jest for end-to-end testing
    
    chore: set up TypeScript configuration for building the project

---
+++ src/products/products.service.ts
@@ -0,0 +1,83 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { PaginationDto } from '../common';
+import { metadata } from 'reflect-metadata/no-conflict';
+
+@Injectable()
+export class ProductsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const product = await this.prisma.product.create({
+      data: createProductDto,
+    });
+
+    return product;
+  }
+
+  async findAll(paginationDto:PaginationDto) {
+
+    const {page, limit} = paginationDto;
+    const totalPages = await  this.prisma.product.count({where:{available:true}});
+    const lastPage = Math.ceil(totalPages/limit!);
+
+
+    return {
+      data: await this.prisma.product.findMany({
+        skip: (page! - 1) * limit!,
+        take:limit,
+        where:{
+          available:true
+        }
+      }),
+
+      metadata:{
+        total:totalPages,
+        page:page,
+        lastPage: lastPage
+      }
+    }
+  }
+
+  async findOne(id: number) {
+    const product = await this.prisma.product.findUnique({
+      where: { id, available: true },
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product with id #${id} not found`);
+    }
+
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+
+    const {id: __,...data} =updateProductDto
+    await this.findOne(id);
+
+    return this.prisma.product.update({
+      where: { id },
+      data: data,
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+   /* return this.prisma.product.delete({
+      where: { id },
+    });*/
+
+    const product = await this.prisma.product.update({
+      where:{id},
+      data:{
+        available:false
+      }
+    });
+
+    return product;
+  }
+}
