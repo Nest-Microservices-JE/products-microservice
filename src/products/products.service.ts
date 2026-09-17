75b9ac3c6eb9388795f01bf49f91c8ad333f0d99
javierestrada26 <javiestrada14@hotmail.com>
2026-09-17 10:22:15 -0500

    feat: update error handling in ProductsService to use RpcException with HttpStatus

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import { PaginationDto } from '../common';
-import { metadata } from 'reflect-metadata/no-conflict';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService {
@@ -47,7 +47,10 @@ export class ProductsService {
     });
 
     if (!product) {
-      throw new NotFoundException(`Product with id #${id} not found`);
+      throw new RpcException({
+        message:`Product with id #${id} not found`,
+        status: HttpStatus.BAD_REQUEST
+      });
     }
 
     return product;
